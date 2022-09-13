ebdc60d2ca185e4d942788157091e2a186a65cf1
Pappu08 <pappuprasad016@gmail.com>
2022-09-13 19:48:52 +0530

    first commit

---
+++ Footer.php
@@ -0,0 +1,19 @@
+
+        <style>
+            .foot{
+                font-size:23px;
+                font-family:Franklin Gothic;
+                color:#808080;
+                background-color:#44cff1;
+                padding-top:15px;
+                padding-bottom:15px;
+                    
+            }
+        </style>
+    
+            <div class="row">
+                <div class="col-sm-12 text-center foot">
+                    @Thank You for visiting our website.
+                </div>
+            </div>
+        
